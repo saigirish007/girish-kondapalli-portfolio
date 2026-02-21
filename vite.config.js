8be7a40ea4dd8a19c793b3dea72e72a1eb788fd5
saigirish007 <saigirish502@gmail.com>
2026-02-21 03:30:06 +0100

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    react({
+      jsxRuntime: "automatic" // ensures JSX works in production
+    })
+  ],
+  base: "./" // fixes blank page paths on Vercel
+});
